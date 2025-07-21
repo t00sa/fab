5bb2ddb2d5248d8c00090fc864c69c79ed9d973b
Sam Clarke-Green <sam.clarke@metoffice.gov.uk>
2025-07-21 21:26:53 +0100

    New implemenation of fab command line tool
    Create a new version of the fab command line tool that can be used
    either on its own or through the python -m feature.  The tool provides
    a common set of command line options, a logging framework, workspace
    setup, and other convenience features.

---
+++ tests/system_tests/cui/test_cui.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+##############################################################################
+# (c) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT
+# which you should have received as part of this distribution
+##############################################################################
+"""
+System tests for the fab command line utility.
+"""
+
+import sys
+import pytest
+from fab.cui.__main__ import main
+
+
+class TestArgsHandling:
+    """Test the command line arguments."""
+
+    def test_args_help(self, capsys):
+        """Test list source of arguments."""
+
+        with pytest.raises(SystemExit) as exc:
+            main(["--help"])
+        assert exc.value.code == 0
+
+        captured = capsys.readouterr()
+        assert "--file FILE" in captured.out
+
+    def test_sys_help(self, capsys, monkeypatch):
+        """Test sys.argv alternate source of arguments."""
+
+        monkeypatch.setattr(sys, "argv", ["__main__.py", "--help"])
+
+        with pytest.raises(SystemExit) as exc:
+            main()
+        assert exc.value.code == 0
+
+        captured = capsys.readouterr()
+        assert "--file FILE" in captured.out
+
+
+class TestUserTarget:
+    """Test with a user-supplied target script."""
+
+    def test_missing_file(self, capsys):
+        """Test with a missing target file."""
+
+        with pytest.raises(SystemExit) as exc:
+            main(["--file", "nosuch"])
+        assert exc.value.code == 2
+
+        captured = capsys.readouterr()
+        assert "error: fab file does not exist" in captured.err
+
+    def test_invalid_class_name(self, tmp_path, capsys):
+        """Test with an invalid build class name."""
+
+        target = tmp_path / "invalid.py"
+
+        with target.open("wt", encoding="utf-8") as fd:
+            print("from fab.target.base import FabTargetBase", file=fd)
+            print("class FabBuildInvalidName(FabTargetBase):", file=fd)
+            print("  def run(self, args):", file=fd)
+            print("    pass", file=fd)
+
+        with pytest.raises(SystemExit) as exc:
+            main(["--file", str(target)])
+        assert exc.value.code == 2
+
+        captured = capsys.readouterr()
+        assert "error: unable to find FabBuildTarget in" in captured.err
+
+    def test_invalid_subclass(self, tmp_path, capsys):
+        """Test with an invalid build class name."""
+
+        target = tmp_path / "invalid.py"
+
+        with target.open("wt", encoding="utf-8") as fd:
+            print("class FabBuildTarget:", file=fd)
+            print("  def run(self, args):", file=fd)
+            print("    pass", file=fd)
+
+        with pytest.raises(SystemExit) as exc:
+            main(["--file", str(target)])
+        assert exc.value.code == 2
+
+        captured = capsys.readouterr()
+        assert "error: FabBuildTarget is not a valid subclass" in captured.err
+
+    def test_missing_project(self, tmp_path):
+        """Test with a simple valid build class."""
+
+        target = tmp_path / "valid.py"
+
+        with target.open("wt", encoding="utf-8") as fd:
+            print("from fab.target.base import FabTargetBase", file=fd)
+            print("class FabBuildTarget(FabTargetBase):", file=fd)
+            print("  def run(self, args):", file=fd)
+            print("    pass", file=fd)
+
+        with pytest.raises(SystemExit) as exc:
+            main(["--file", str(target)])
+
+        assert exc.value.code == 10
+
+    def test_valid_file(self, tmp_path):
+        """Test with a simple valid build class."""
+
+        target = tmp_path / "valid.py"
+
+        with target.open("wt", encoding="utf-8") as fd:
+            print("from fab.target.base import FabTargetBase", file=fd)
+            print("class FabBuildTarget(FabTargetBase):", file=fd)
+            print("  project_name = 'testing'", file=fd)
+            print("  def run(self, args):", file=fd)
+            print("    pass", file=fd)
+
+        main(["--file", str(target)])
+
+    def test_default_file(self, tmp_path, capsys, monkeypatch):
+        """Test with a valid default file."""
+
+        target = tmp_path / "FabFile"
+
+        with target.open("wt", encoding="utf-8") as fd:
+            print("from fab.target.base import FabTargetBase", file=fd)
+            print("class FabBuildTarget(FabTargetBase):", file=fd)
+            print("  project_name = 'testing'", file=fd)
+            print("  def run(self, args):", file=fd)
+            print("    pass", file=fd)
+
+        with monkeypatch.context() as mp:
+            mp.chdir(tmp_path)
+            main([])
+
+        captured = capsys.readouterr()
+
+        assert captured.err == ""
+        assert captured.out == ""
